2a2b41d5cb01aa0817094e94cc078cdc02113aaa
Krishna Sree Koushik <krishnasreekoushik@gmail.com>
2025-12-29 14:30:17 +0530

    Remove Certifications tab from AboutSection
    Removed the Certifications tab button from AboutSection.

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -125,12 +125,7 @@ const AboutSection = () => {
             >
               Education
             </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
-              Certifications
-            </TabButton>
+            
           </div>
           <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
         </div>
