07f5101399243249e0d6e8ab85b6acd9b438d022
Krishna Sree Koushik <krishnasreekoushik@gmail.com>
2026-01-14 00:39:11 +0530

    Fix image path for Netflix project

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -36,10 +36,11 @@ const projectsData = [
     id: 4,
     title: "Netflix-Clone",
     description: "Netflix-like OTT web app built with React.js, Firebase authentication, React Router, and Axios, featuring reusable components and a scalable UI.",
-    image:  "public/images/projects/Netflix-img.jpg",
+    image:  "/images/projects/Netflix-img.jpg",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/GKSKoushik/NetflixClone",
     previewUrl: "/",
+    
   }
 ];
 
